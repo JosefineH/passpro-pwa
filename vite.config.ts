652e0a4f29c835072289f5db0b8d1aee21d611ac
Josefine <josefine.holling@gmail.com>
2025-03-16 13:52:09 +0100

    try two new vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,6 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/passpro-pwa',
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: 'My PWA',
-        short_name: 'PWA',
-        description: 'My Progressive Web App',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  base: '/passpro-pwa/',
+  plugins: [react()],
 })
